fe47dd4bf55978f908813f1d1c8bead116dafb71
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2018-11-23 09:35:27 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,7 @@
+## AoC attempt in Javascript
+
+
+Uses a bunch of JavaScript stuff
+ - https://developers.google.com/web/updates/2017/11/dynamic-import
+ 
+ 
---
+++ src/common/assert.js
@@ -0,0 +1,5 @@
+export function assertEquals(left, right) {
+    if(left !== right) {
+        console.log(`Expected ${left} is not equal ${right}`);
+    }
+}
