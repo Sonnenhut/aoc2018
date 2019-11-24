883f0f5559c1d12ca36c8484de25f5b7035232dc
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-11-24 11:23:43 +0100

    Day 20: index page

--- day20/index.js
+++ day20/index.js
@@ -1,6 +1,5 @@
 import {fileAsText} from "../common/files.js";
-import {EXAMPLE, Area} from "./day20_a.js";
-import {EXAMPLE_23,EXAMPLE_31,EXAMPLE_18,} from "./day20_a.js";
+import {Area} from "./day20_a.js";
 
 
 class Day20 extends HTMLElement {
@@ -25,41 +24,18 @@ class Day20 extends HTMLElement {
                     transform: scaleY(-1);
                 }
             </style>
-            <svg height="500" width="500" viewBox="-50 -50 100 100"></svg>`;
-        //const parsed = parse(EXAMPLE_23);
-        //parsed.forEach(node => this.render(node))
-        //allNodes(EXAMPLE_23).forEach(node => this.renderNode(node))
+            <svg height="1000" width="1000" viewBox="-800 -800 1500 1500"></svg>`;
+        fileAsText('day20/input.txt').then(input => {
+            const area = new Area(input);
+            area.nodes.forEach(node => this.render(node));
+        });
     }
 
     render(node) {
         const enlarge = 10;
         let svgNS = "http://www.w3.org/2000/svg";
         const svg = document.querySelector('svg');
-        const toRender = [node];
-        const rendered = [];
-        while(toRender.length) {
-            let curr = toRender.pop();
-            (curr.prev || []).forEach(nextNode => {
-                let line = document.createElementNS(svgNS, 'line');
-                line.setAttribute( "x1", curr.x * enlarge);
-                line.setAttribute( "y1", curr.y * enlarge);
-                line.setAttribute( "x2", nextNode.x * enlarge);
-                line.setAttribute( "y2", nextNode.y * enlarge);
-                svg.append(line);
-                //if(!rendered.filter(other => other.x === nextNode.x && other.y === nextNode.y).length) {
-                if(!rendered.includes(nextNode)){
-                    toRender.push(nextNode);
-                    rendered.push(curr);
-                }
-            });
-        }
-    }
-
-    renderNode(node) {
-        const enlarge = 10;
-        let svgNS = "http://www.w3.org/2000/svg";
-        const svg = document.querySelector('svg');
-        (node.prev || []).forEach(prev => {
+        (node.next || []).forEach(prev => {
             let line = document.createElementNS(svgNS, 'line');
             line.setAttribute( "x1", node.x * enlarge);
             line.setAttribute( "y1", node.y * enlarge);
