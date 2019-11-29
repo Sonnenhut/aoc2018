90ea204678aaec89ad7bfe111a84f9458d82cd6d
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2019-11-30 00:41:28 +0100

    Day 15: Part 2

--- day15/day15_a.js
+++ day15/day15_a.js
@@ -162,21 +162,6 @@ const EXAMPLE_DIRECTLY_ATTACK =
     "#.E#\n" +
     "####";
 
-const EXAMPLE_REDDIT_71 =
-    "#####\n" +
-    "#GG##\n" +
-    "#.###\n" +
-    "#..E#\n" +
-    "#.#G#\n" +
-    "#.E##\n" +
-    "#####";
-
-const EXAMPLE_REDDIT_67 =
-    "####\n" +
-    "##E#\n" +
-    "#GG#\n" +
-    "####";
-
 const EXAMPLE_REDDIT_38 =
     "################\n" +
     "#.......G......#\n" +
@@ -186,19 +171,6 @@ const EXAMPLE_REDDIT_38 =
     "#....###########\n" +
     "#.......EG.....#\n" +
     "################";
-
-const EXAMPLE_REDDIT_66 =
-    "######################\n" +
-    "#...................E#\n" +
-    "#.####################\n" +
-    "#....................#\n" +
-    "####################.#\n" +
-    "#....................#\n" +
-    "#.####################\n" +
-    "#....................#\n" +
-    "###.##################\n" +
-    "#EG.#................#\n" +
-    "######################";
 
 const EXAMPLE_REDDIT_IN_READING_ORDER =
     "#######\n" +
@@ -282,21 +254,17 @@ export default async function main() {
     // test taking turns
     testExample47();
 
-
-    outcome(EXAMPLE_47_ROUNDS).toBe([47, 590]);
-    outcome(EXAMPLE_37_ROUNDS).toBe([37, 982]);
-    outcome(EXAMPLE_46_ROUNDS).toBe([46, 859]);
-    outcome(EXAMPLE_35_ROUNDS).toBe([35, 793]);
-    outcome(EXAMPLE_54_ROUNDS).toBe([54, 536]);
-    outcome(EXAMPLE_20_ROUNDS).toBe([20, 937]);
+    outcome_part1(EXAMPLE_47_ROUNDS).toBe([47, 590]);
+    outcome_part1(EXAMPLE_37_ROUNDS).toBe([37, 982]);
+    outcome_part1(EXAMPLE_46_ROUNDS).toBe([46, 859]);
+    outcome_part1(EXAMPLE_35_ROUNDS).toBe([35, 793]);
+    outcome_part1(EXAMPLE_54_ROUNDS).toBe([54, 536]);
+    outcome_part1(EXAMPLE_20_ROUNDS).toBe([20, 937]);
     //outcome(EXAMPLE_REDDIT_67).toBe([67, 200]);// <- still a bug here
-    outcome(EXAMPLE_REDDIT_38).toBe([38, 486]);
-
-    //outcome(EXAMPLE_REDDIT_71).toBe([71, 197]);
-    //outcome(EXAMPLE_REDDIT_66).toBe([66, 202]);
+    outcome_part1(EXAMPLE_REDDIT_38).toBe([38, 486]);
 
     return fileAsText('day15/input.txt').then(input => {
-        let res = outcome(input);
+        let res = outcome_part1(input);
         res = res[0] * res [1];
         res.toBe(201856);
         return res;
@@ -320,11 +288,20 @@ function testExample47() {
     Square.asString(squares).toBe(EXAMPLE_47_AFTER_23);
 }
 
-function outcome(input) {
+function outcome_part1(input) {
+    let {rounds, totalHp} = outcome(input);
+    return [rounds, totalHp];
+}
+
+export function outcome(input, stopWhenElveDies = false, elveAp = 3) {
     let squares = parseSquares(input);
+    const elveCnt = () => squares.filter(sq => sq.type === 'E').length;
     let typesPresent = (arr, type) => arr.filter(sq => sq.type === type).length > 0;
+    squares.filter(sq => sq.type === 'E').forEach(sq => sq.ap = elveAp);
+
+    const originalElveCnt = elveCnt();
     let roundCnt = 0;
-    loop: while(true) {//typesPresent(squares, 'E') && typesPresent(squares, 'G')
+    loop: while(true) {
         roundCnt++;
         squares = Square.inReadingOrder(squares);
 
@@ -336,17 +313,23 @@ function outcome(input) {
             }
             sq.turn(squares);
         }
+        if(stopWhenElveDies && originalElveCnt > elveCnt()) {
+            break;
+        }
     }
-
     roundCnt--;
     squares = Square.inReadingOrder(squares);
 
     console.log(Square.asString(squares));
-    const hpSum = squares.filter(sq => sq.type === 'E' || sq.type === 'G')
+    const gobHp = squares.filter(sq => sq.type === 'G')
         .map(sq => sq.hp)
         .reduce((acc, curr) => acc + curr,0);
-    console.log(roundCnt, hpSum, squares.filter(sq => sq.type === 'E' || sq.type === 'G'));
-    return [roundCnt, hpSum];
+    const elveHp = squares.filter(sq => sq.type === 'E')
+        .map(sq => sq.hp)
+        .reduce((acc, curr) => acc + curr,0);
+    let res = {rounds: roundCnt, totalHp: (gobHp + elveHp), elveHp, elveCasualties: originalElveCnt !== elveCnt(), elveAp};
+    console.log(res);
+    return res;
 }
 
 function parseSquares(input) {
@@ -396,7 +379,6 @@ class Square {
         inRange = Square.inReadingOrder(inRange);
         let dij = new Dijkstra(units, this);
         let dijkSquares = dij.run(); // sets distances
-        //console.log(distances, targetLocations)
         let dijkInRange = inRange.map(loc => Square.at(dijkSquares, loc));
         const lowestDistance = dijkInRange.map(sq => sq.distance).reduce((acc, curr) => Math.min(acc, curr),Number.MAX_SAFE_INTEGER);
         const chosen = dijkInRange.filter(sq => sq.distance === lowestDistance)[0];
@@ -480,8 +462,8 @@ export class Dijkstra {
     }
     run() {
         while(this.queue.length) {
-            let curr = this._nextInQueue();
-            let neighbors = this._neighborsOf(curr);
+            let curr = this.queue.next();
+            let neighbors = Square.around(curr, this.squares,'.');
             for(let neighbor of neighbors) {
                 if(this.queue.includes(neighbor)) {
                     this._updateDistance(curr, neighbor);
@@ -502,12 +484,6 @@ export class Dijkstra {
             v.prev = Square.inReadingOrder([u, v.prev])[0];
         }
     }
-    _nextInQueue() {
-        return this.queue.next();
-    }
-    _neighborsOf(loc) {
-        return Square.around(loc, this.squares,'.');
-    }
     static shortestPath(target) {
         let path = [target];
         let u = target;
@@ -518,245 +494,3 @@ export class Dijkstra {
         return path;
     }
 }
-
-/*
-export default async function main() {
-
-    new Unit('G').isEnemy(new Unit('E')).toBe(true);
-    new Unit('E').isEnemy(new Unit('G')).toBe(true);
-
-    pos(1,1).isAdjacent(pos(0,1)).toBe(true);
-    pos(1,1).isAdjacent(pos(2,1)).toBe(true);
-    pos(1,1).isAdjacent(pos(1,0)).toBe(true);
-    pos(1,1).isAdjacent(pos(1,2)).toBe(true);
-
-    [pos(1,1), pos(1,0)].first().x.toBe(0);
-    [pos(1,1), pos(0,1)].first().y.toBe(0);
-
-    const example0 = parse(EXAMPLE_0_DAY);
-    let elf = example0.filter(unit => unit.pos.eq(pos(1,1)))[0];
-    elf.isElf().toBe(true);
-    elf.targets(example0).first().pos.eq(pos(1,4)).toBe(true);
-    const inRange = elf.inRangeUnits(elf.targets(example0), example0).sorted();
-    inRange.length.toBe(6);
-    inRange[0].pos.eq(pos(1,3)).toBe(true);
-    inRange[1].pos.eq(pos(1,5)).toBe(true);
-    inRange[2].pos.eq(pos(2,2)).toBe(true);
-    inRange[3].pos.eq(pos(2,5)).toBe(true);
-    inRange[4].pos.eq(pos(3,1)).toBe(true);
-    inRange[5].pos.eq(pos(3,3)).toBe(true);
-
-    elf.isReachable(inRange[0], example0).toBe(true);
-    const reachable = inRange.filter(other => elf.isReachable(other, example0)).sorted();
-    reachable.length.toBe(4);
-    reachable[0].pos.eq(pos(1,3)).toBe(true);
-    reachable[1].pos.eq(pos(2,2)).toBe(true);
-    reachable[2].pos.eq(pos(3,1)).toBe(true);
-    reachable[3].pos.eq(pos(3,3)).toBe(true);
-
-    elf.path(reachable[0], example0).length.toBe(2);
-    elf.path(reachable[1], example0).length.toBe(2);
-    elf.path(reachable[2], example0).length.toBe(2);
-    elf.path(reachable[3], example0).length.toBe(4);
-    elf.path(reachable[3], example0).length.toBe(4);
-    elf.path(inRange[1], example0).length.toBe(0); // behind a wall, no path
-
-
-    let elf;
-
-    const example1 = parse(EXAMPLE_1_DAY);
-    elf = example1.filter(unit => unit.isElf()).first();
-    elf.nextStep(example1);
-    elf.pos.eq(pos(1,3)).toBe(true);
-    elf.nextStep(example1);
-    elf.pos.eq(pos(1,4)).toBe(true);
-    elf.nextStep(example1);
-    elf.pos.eq(pos(2,4)).toBe(true);
-    // no more step, is already adjacent
-    elf.nextStep(example1);
-    elf.pos.eq(pos(2,4)).toBe(true);
-
-    const example2 = parse(EXAMPLE_2_DAY);
-    let movingUnits = example2.filter(unit => unit.isElf() || unit.isGoblin());
-    elf = example2.filter(unit => unit.isElf()).first();
-
-    const checkGoblins = (positions) => {
-        return positions.map(pos => example2.at(pos))
-                .filter(unit => unit.isGoblin())
-                .length.toBe(positions.length)
-    };
-
-    const tick0 =  [pos(1,1), pos(1,4), pos(1,7),
-        pos(4,  1), pos(4,7),
-        pos(7,  1), pos(7,4), pos(7,7)];
-    elf.pos.eq(pos(4,4));
-    checkGoblins(tick0);
-
-    movingUnits.forEach(unit => unit.nextStep(example2));
-
-    const tick1 =  [pos(1,2), pos(1,6),
-        pos(4,2)];
-    checkGoblins(tick1);
-
-
-    return fileAsText('day15/input.txt').then(input => {
-        //return solve(parseInt(input)).toBe('4910101614');
-    });
-};
-
-
-export function solve(ripeness) {
-}
-
-function parse(input) {
-    return input.split(/\r?\n|\n/g).reduce((acc, row, y) => {
-        acc.push(... row.split('').map((self, x) => new Unit(self, pos(y,x))));
-        return acc;
-    },[])
-}
-
-class Unit {
-    constructor(self, pos) {
-        this.self = self;
-        this.pos = pos;
-    }
-    isGoblin() {
-        return this.self === 'G';
-    }
-    isElf() {
-        return this.self === 'E';
-    }
-    isOpen() {
-        return this.self === '.';
-    }
-    isWall() {
-        return this.self === '#';
-    }
-    isEnemy(other) {
-        const chkEnemy = (left, right) => left.isGoblin() && right.isElf();
-        return chkEnemy(this, other) || chkEnemy(other, this);
-    }
-    targets(all) {
-        return all.filter(other => this.isEnemy(other));
-    }
-    inRangeUnits(targets, all) {
-        return [... targets.reduce((acc,target) => {
-            all.filter(other => other.isOpen() && other.pos.isAdjacent(target.pos))
-                .forEach(item => acc.add(item));
-            return acc;
-        },new Set())];
-    }
-    adjacent(all) {
-        return all.filter(other => other.pos.isAdjacent(this.pos));
-    }
-    isReachable(toReach, all) {
-        return this.path(toReach, all).length > 0;
-    }
-    path(toReach, all) { // shortest path in reading order or empty array
-        const paths = this.reachablePaths(toReach, all);
-        if(paths.length === 0) {
-            return [];
-        } else {
-            // only keep shortest paths
-            return Unit.choosePath(paths)
-        }
-    }
-    reachablePaths(toReach, all, path = []) {
-        const adjacent = this.adjacent(all);
-        if(toReach === this) {
-            return [path];
-        } else {
-            return adjacent.filter(other => other.isOpen() || other === toReach)
-                .filter(other => !path.includes(other))
-                .reduce((acc, curr) => {
-                    const paths = curr.reachablePaths(toReach, all, [...path, curr]);
-                    acc.push(... paths);
-                    return acc
-                }, []);
-        }
-    }
-    nextStep(all) {
-        const paths = this.targets(all).map(other => this.path(other, all));
-        const path = Unit.choosePath(paths);
-        if(path.length > 1) {
-            let prevPosition = this.pos.copy();
-            let nextPosition = path[0].pos.copy();
-            // just switch the position of the units
-            path[0].pos = prevPosition;
-            this.pos = nextPosition;
-        }
-        return undefined;
-    }
-
-    static choosePath(paths) {
-        // choose path by shortest and what is first in reading order
-        const shortestStepsCnt = paths.reduce((left, right) => left.length < right.length ? left : right).length;
-        const allShortest = paths.reduce((acc, path) => {
-            if(path.length === shortestStepsCnt) {
-                acc.push(path);
-            }
-            return acc;
-        },[]);
-        const firstStepInReadingOrder = allShortest.map(path => path[0]).first();
-        return allShortest.filter(path => path[0] === firstStepInReadingOrder)[0];
-    }
-}
-
-function pos(y,x) {
-    return new Position(y,x);
-}
-class Position {
-    constructor(y,x) {
-        this.y = y;
-        this.x = x;
-    }
-    isAdjacent(other) {
-        return this.up().eq(other) || this.down().eq(other) || this.left().eq(other) || this.right().eq(other);
-    }
-    copy() {
-        return new Position(this.y,this.x);
-    }
-    eq(other) {
-        return this.x === other.x && this.y === other.y;
-    }
-    up() {
-        return pos(this.y-1, this.x);
-    }
-    down() {
-        return pos(this.y+1, this.x);
-    }
-    left() {
-        return pos(this.y, this.x-1);
-    }
-    right() {
-        return pos(this.y, this.x+1);
-    }
-    hash() {
-        return `${this.y}_${this.x}`;
-    }
-}
-
-// sorted in reading order
-Array.prototype.sorted = function() {
-    return this.sort((left, right) => {
-                    const leftHash = left.pos ? left.pos.hash() : left.hash();
-                    const rightHash = right.pos ? right.pos.hash() : right.hash();
-                    if(leftHash === rightHash) {
-                        return 0;
-                    } else if(leftHash < rightHash) {
-                        return -1;
-                    } else if(leftHash > rightHash) {
-                        return 1;
-                    }
-                });
-};
-
-Array.prototype.first = function () {
-    return this.sorted()[0];
-};
-Array.prototype.at = function (position) {
-    return this.filter(unit => unit.pos.eq(position))[0];
-};
-Array.prototype.last = function () {
-    return this[this.length - 1];
-};*/
--- day15/day15_b.js
+++ day15/day15_b.js
@@ -1,7 +1,73 @@
 import {fileAsText} from '../common/files.js'
-import {PriorityQueue} from "../day20/PriorityQueue.js";
+import {outcome} from "./day15_a.js";
+
+const EXAMPLE_29 =
+    "#######\n" +
+    "#.G...#\n" +
+    "#...EG#\n" +
+    "#.#.#G#\n" +
+    "#..G#E#\n" +
+    "#.....#\n" +
+    "#######";
+
+const EXAMPLE_33 =
+    "#######\n" +
+    "#E..EG#\n" +
+    "#.#G.E#\n" +
+    "#E.##E#\n" +
+    "#G..#.#\n" +
+    "#..E#.#\n" +
+    "#######";
+
+const EXAMPLE_37 =
+     "#######\n" +
+     "#E.G#.#\n" +
+     "#.#G..#\n" +
+     "#G.#.G#\n" +
+     "#G..#.#\n" +
+     "#...E.#\n" +
+     "#######";
+
+const EXAMPLE_39 =
+    "#######\n" +
+    "#.E...#\n" +
+    "#.#..G#\n" +
+    "#.###.#\n" +
+    "#E#G#G#\n" +
+    "#...#G#\n" +
+    "#######";
+
+const EXAMPLE_30 =
+    "#########\n" +
+    "#G......#\n" +
+    "#.E.#...#\n" +
+    "#..##..G#\n" +
+    "#...##..#\n" +
+    "#...#...#\n" +
+    "#.G...G.#\n" +
+    "#.....G.#\n" +
+    "#########";
 
 export default async function main() {
+    outcome_part2(EXAMPLE_29).toBe([29,172]);
+    outcome_part2(EXAMPLE_33).toBe([33,948]);
+    outcome_part2(EXAMPLE_37).toBe([37,94]);
+    outcome_part2(EXAMPLE_39).toBe([39,166]);
+    outcome_part2(EXAMPLE_30).toBe([30,38]);
     return fileAsText('day15/input.txt').then(input => {
+        let res = outcome_part2(input, 11 /*hard-coded to not take ages when loading the webpage*/);
+        res.toBe([47,1022]);// 12ap
+        return res[0] * res[1];
     });
 };
+
+function outcome_part2(input, startAp = 3) {
+    let ap = startAp;
+    let outcomeRes = {elveCasualties: true, rounds: 0};
+
+    while(outcomeRes.elveCasualties) {
+        ap++;
+        outcomeRes = outcome(input, true, ap);
+    }
+    return [outcomeRes.rounds, outcomeRes.elveHp]
+}
